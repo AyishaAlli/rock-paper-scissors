28236779505ffd068a6c91642a1cc60c322e8e97
Ayisha Alli <alli.ayisha1@gmail.com>
2023-01-23 13:13:12 +0000

    game play

--- app.js
+++ app.js
@@ -4,22 +4,40 @@ function getComputerChoice() {
 }
 
 //console.log(getComputerChoice());
-
+const scores = {
+  computer: 0,
+  player: 0,
+};
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
+  playerSelection = playerSelection.toLowerCase();
   if (playerSelection == computerSelection) {
     return `You chose: ${playerSelection}\nComputer chose: ${computerSelection} \n\nTIE!`;
   } else if (playerSelection == "rock" && computerSelection == "paper") {
+    scores.computer += 1;
     return `You chose: ${playerSelection}\nComputer chose: ${computerSelection} \n\nComputer Wins`;
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    scores.computer += 1;
     return `You chose: ${playerSelection}\nComputer chose: ${computerSelection} \n\nComputer Wins`;
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    scores.computer += 1;
     return `You chose: ${playerSelection}\nComputer chose: ${computerSelection} \n\nComputer Wins`;
   } else {
+    scores.player += 1;
     return `You chose: ${playerSelection}\nComputer chose: ${computerSelection} \n\nPlayer Wins`;
   }
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "Rock";
+// const computerSelection = getComputerChoice();
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+//console.log(playRound(playerSelection, computerSelection));
+console.log(game());
+console.log(scores);
